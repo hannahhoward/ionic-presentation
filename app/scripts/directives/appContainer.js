5fd679c9a0cafad12c60a53d38573144cdc56ff1
Hannah Howard <hannah@hannahhoward.net>
2014-08-03 14:19:53 -0700

    Scope resize

--- app/scripts/directives/appContainer.js
+++ app/scripts/directives/appContainer.js
@@ -4,6 +4,31 @@ angular.module('famousAngularStarter')
   .directive('appContainer', function ($famous, $keyframeAnimation) {
     function link(scope, element, attrs) {
       var Easing = $famous['famous/transitions/Easing'];
+      var Engine = $famous['famous/core/Engine'];
+
+      var faAppIsolate = $famous.find("#app_container")[0];
+      scope.mainContext = faAppIsolate.context;
+
+      scope.resize = function(dimensions) {
+        var width = dimensions[0];
+        var height = dimensions[1];
+        var idealWidth = 1440;
+        var idealHeight = 900;
+        var widthRatio = width / idealWidth;
+        var heightRatio = height / idealHeight;
+        var scale = (widthRatio < heightRatio ? widthRatio : heightRatio);
+
+        scope.globalScale = function() {
+          return [scale, scale, 1];
+        }
+      }
+
+      Engine.on('resize',function(){
+        var dimensions = scope.mainContext.getSize();
+        scope.resize(dimensions);
+      });
+
+      scope.resize(scope.mainContext.getSize())
 
       scope.globalTranslate = $keyframeAnimation(
       [
